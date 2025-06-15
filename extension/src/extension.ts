fc73aefb2bd07ebd7c8b143ca6fd90792375e1bf
Manan Vyas <161738446+MananVyas01@users.noreply.github.com>
2025-06-15 08:41:55 +0000

    Resove the Console Log Issue in lint

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,13 +1,18 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('DevScope extension is now active!');
+  const outputChannel = vscode.window.createOutputChannel('DevScope');
+  outputChannel.appendLine('DevScope extension is now active!');
+  outputChannel.show();
 
-	let disposable = vscode.commands.registerCommand('devscope.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from DevScope!');
-	});
+  let disposable = vscode.commands.registerCommand(
+    'devscope.helloWorld',
+    () => {
+      vscode.window.showInformationMessage('Hello World from DevScope!');
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
